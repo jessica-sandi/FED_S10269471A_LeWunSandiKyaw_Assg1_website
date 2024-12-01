fdab0d3ff8210270d769112231795ddc2a58483b
jessica-sandi <s10269471@connect.np.edu.sg>
2024-12-01 21:11:10 +0800

    updated media queries and othes

--- javascript.js
+++ javascript.js
@@ -13,29 +13,5 @@ $(document).ready(function(){
   })
 
 
-function position(id){
-  var card = document.getElementsByClassName('card')[id];
-  // card.style.transform = 'scale(1.5)';
-  console.log(id)
-}
-
-document.querySelectorAll('.movie').forEach(movie => {
-  const video = movie.querySelector('video');
-
-  // Play video on hover
-  movie.addEventListener('mouseenter', () => {
-    if (video) {
-      video.play();
-    }
-  });
-
-  // Pause video when hover ends
-  movie.addEventListener('mouseleave', () => {
-    if (video) {
-      video.pause();
-      video.currentTime = 0; // Reset video to the beginning
-    }
-  });
-});
 
 
